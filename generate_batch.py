d1cdc65c79bb2ba6fac9d66f76282eb6c34f604f
Athena <bqh1402003@gmail.com>
2024-05-27 17:13:41 +0700

    feat: add equipment choices

--- generate_batch.py
+++ generate_batch.py
@@ -1,9 +1,26 @@
-input_filename = input('Enter the name of the weapon type to generate batch: ')
-output_filename = input('Enter the name of the output file: ')
+config_type = input('Enter the type of config you want to generate batch for (artifacts or weapons): ')
+if config_type == 'artifacts':
+    input_filename = input('Enter the name of the artifacts file to generate batch: ')
+    output_filename = input('Enter the name of the output file: ')
+    with open(f'./artifacts/{input_filename}.txt', 'r') as f:
+        words = f.read().split()
+    with open(f'{output_filename}_output.txt', 'w') as f:
+        for word in words:
+            f.write('.\\run_optimizer_full.bat "{}.txt"\n'.format(word))
+elif config_type == 'weapons':
+    input_filename = input('Enter the name of the weapon type to generate batch: ')
+    output_filename = input('Enter the name of the output file: ')
+    with open(f'./weapons/{input_filename}.txt', 'r') as f:
+        words = f.read().split()
+    with open(f'{output_filename}_output.txt', 'w') as f:
+        for word in words:
+            f.write('.\\run_optimizer_full.bat "{}.txt"\n'.format(word))
+# input_filename = input('Enter the name of the weapon type to generate batch: ')
+# output_filename = input('Enter the name of the output file: ')
 
-with open(f'./weapons/{input_filename}.txt', 'r') as f:
-    words = f.read().split()
+# with open(f'./weapons/{input_filename}.txt', 'r') as f:
+#     words = f.read().split()
 
-with open(f'{output_filename}_output.txt', 'w') as f:
-    for word in words:
-        f.write('.\\run_optimizer_full.bat "{}.txt"\n'.format(word))
+# with open(f'{output_filename}_output.txt', 'w') as f:
+#     for word in words:
+#         f.write('.\\run_optimizer_full.bat "{}.txt"\n'.format(word))
